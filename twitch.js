403e75b1d61c7a121916de604c4a3a86cec4c32e
lvndry <monga.4landry@gmail.com>
2018-01-03 10:09:14 +0100

    datas available

---
+++ twitch.js
@@ -0,0 +1,53 @@
+let channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
+let api = "https://wind-bow.glitch.me/twitch-api";
+let site = "https://www.twitch.tv/";
+let table = $("table");
+
+function getStatus(type, name) {
+  return fetch(api + "/" + type + "/" + name, {
+    method: "GET",
+    mode: "cors"
+  }).then(resp => {
+    if (resp.status != 200) {
+      console.log("Looks like there is a problem. Status code: " + resp.status);
+      return;
+    }
+    return resp.json().then(json => {
+      console.log(json);
+      return (json.stream === null) ? "Offline" : "Online";
+    });
+  });
+}
+
+function getChannels(type, channels) {
+  $.each(channels, function(index, name) {
+    let promise = getStatus(type, name);
+    promise.then(function(s) {
+      let tr = $("<tr>");
+      let chn = $("<td>");
+      let status = $("<td>");
+      chn.text(name);
+      status.text(s);
+      tr.prop("class", s.toLowerCase());
+      tr.append(chn);
+      tr.append(status);
+      table.append(tr);
+    })
+  });
+  $(".channels").append(table);
+}
+
+$(".status").click(function(event) {
+  let st = event.target.className;
+  console.log(st);
+  if(st !== "all"){
+    st = "." + st;
+    console.log(st);
+    $("tr").hide();
+    $(st).show();
+  }
+  else $("tr").show();
+});
+$(document).ready(function() {
+  getChannels("streams", channels);
+});
